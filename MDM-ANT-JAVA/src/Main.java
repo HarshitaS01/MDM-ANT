97c3e944826efcefb048c2fe2abfa90af5df7690
Harshita Singh <2023.harshita.singh@ves.ac.in>
2024-10-13 08:30:07 +0530

    added a line

--- MDM-ANT-JAVA/src/Main.java
+++ MDM-ANT-JAVA/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         // Correct method to print a line of text
-        System.out.println("Hello and welcome!"); // using println instead of printf
+        System.out.println("Hello and welcome! MDM USING ANT TO CREATE A PIPIELINE"); // using println instead of printf
     }
 }
