add28ca311cfb15282fb5adb8401c11a9e43980e
Harshita Singh <2023.harshita.singh@ves.ac.in>
2024-10-13 08:18:56 +0530

    created hello world java file

---
+++ MDM-ANT-JAVA/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // Correct method to print a line of text
+        System.out.println("Hello and welcome!"); // using println instead of printf
+    }
+}
